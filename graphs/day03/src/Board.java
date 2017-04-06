490d010a843e62c61f670fab5fd37361c199292e
David Abrahams <davidabrahams@sbcglobal.net>
2017-04-05 22:51:18 -0400

    resetting neighbors

--- graphs/day03/src/Board.java
+++ graphs/day03/src/Board.java
@@ -10,8 +10,9 @@ public class Board {
     private int n;
     public int[][] tiles;
     private int[][] goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
-    public int[][] goalPoints = {{2,2},{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1}}; //index is key; 0 is actually where 9 would go
-    public int[] blankPoint = {0,0};
+    public int[][] goalPoints = {{2, 2}, {0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2, 0}, {2, 1}}; //index is
+    // key; 0 is actually where 9 would go
+    public int[] blankPoint = {0, 0};
 
     /*
      * Set the global board size and tile state
@@ -33,7 +34,7 @@ public class Board {
                 s += i + " ";
             s += "; ";
         }
-        return s.substring(0, s.length()-2);
+        return s.substring(0, s.length() - 2);
     }
 
     /*
@@ -41,7 +42,7 @@ public class Board {
      * class should  work for any puzzle size)
      */
     private int size() {
-    	// TODO: Your code here
+        // TODO: Your code here
         return tiles.length;
     }
 
@@ -50,7 +51,7 @@ public class Board {
      * Estimated cost from the current node to the goal for A* (h(n))
      */
     public int manhattan() {
-		// TODO: Your code here
+        // TODO: Your code here
         int sum = 0;
         for (int row = 0; row < size(); row++) {
             for (int col = 0; col < size(); col++) {
@@ -72,7 +73,7 @@ public class Board {
      * Compare the current state to the goal state
      */
     public boolean isGoal() {
-    	// TODO: Your code here
+        // TODO: Your code here
         for (int row = 0; row < size(); row++) {
             for (int col = 0; col < size(); col++) {
                 if (tiles[row][col] != goal[row][col]) {
@@ -87,8 +88,9 @@ public class Board {
      * Returns true if the board is solvable
      */
     public boolean solvable() { //TODO: *** WE ADDED THE PARAMETER HERE ***
-    	// TODO: Your code here
-        // A pair of tiles form an inversion if the the values on tiles are in reverse order of their appearance in goal state.
+        // TODO: Your code here
+        // A pair of tiles form an inversion if the the values on tiles are in reverse order of their appearance in
+        // goal state.
         // It is not possible to solve an instance of 8 puzzle if number of inversions is odd in the input state.
         int[] tilesArr = boardToArray(tiles);
         int inversions = 0;
@@ -108,47 +110,63 @@ public class Board {
      * blank space up, down, left, and right, and if the resulting board state
      * is valid, add it to an accumulator.
      */
-    public List<Board> neighbors() {
-        // Find the blank tile
-        int i0 = 0, j0 = 0;
-        boolean found = false;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (tiles[i][j] == 0) {
-                    i0 = i;
-                    j0 = j;
-                    found = true;
-                    break;
-                }
-            }
-            if (found) {
-                break;
-            }
+    /*
+     * Return the neighboring boards in the state tree
+     * One possible method: Create a duplicate board state, try moving the
+     * blank space up, down, left, and right, and if the resulting board state
+     * is valid, add it to an accumulator.
+     */
+    public Iterable<Board> neighbors() {
+        // TODO: Your code here
+        ArrayList<Board> neighborFriends = new ArrayList<>();
+        int blankX = blankPoint[0];
+        int blankY = blankPoint[1];
+
+        //check up
+        if (blankY > 0) {
+            int[][] duplicateTilesUp = copyOf(tiles);
+            int swapVal = duplicateTilesUp[blankX][blankY - 1];
+            duplicateTilesUp[blankX][blankY - 1] = 0;
+            duplicateTilesUp[blankX][blankY] = swapVal;
+            Board boardUp = new Board(duplicateTilesUp);
+            neighborFriends.add(boardUp);
+
         }
 
-        // List of accumulated neighbors
-        List<Board> boards = new LinkedList<>();
+        //check left
+        if (blankX > 0) {
+            int[][] duplicateTilesLeft = copyOf(tiles);
+            int swapVal = duplicateTilesLeft[blankX - 1][blankY];
+            duplicateTilesLeft[blankX - 1][blankY] = 0;
+            duplicateTilesLeft[blankX][blankY] = swapVal;
+            Board boardLeft = new Board(duplicateTilesLeft);
+            neighborFriends.add(boardLeft);
+        }
 
-        // Create a new instance of the current board
-        Board board = new Board(tiles);
+        //check down
+        if (blankY < 2) {
+            int[][] duplicateTilesDown = copyOf(tiles);
+            int swapVal = duplicateTilesDown[blankX][blankY + 1];
+            duplicateTilesDown[blankX][blankY + 1] = 0;
+            duplicateTilesDown[blankX][blankY] = swapVal;
+            Board boardDown = new Board(duplicateTilesDown);
+            neighborFriends.add(boardDown);
 
-        /* Edit the new board with each possible swap
-         * If swap was successful, add new board state to the list of neighbors
-         */
-        if (board.swap(i0, j0, i0 - 1, j0))
-            boards.add(board);
-        board = new Board(tiles);
-        if (board.swap(i0, j0, i0, j0 - 1))
-            boards.add(board);
-        board = new Board(tiles);
-        if (board.swap(i0, j0, i0 + 1, j0))
-            boards.add(board);
-        board = new Board(tiles);
-        if (board.swap(i0, j0, i0, j0 + 1))
-            boards.add(board);
+        }
 
-        return boards;
+        //check right
+        if (blankX < 2) {
+            int[][] duplicateTilesRight = copyOf(tiles);
+            int swapVal = duplicateTilesRight[blankX + 1][blankY];
+            duplicateTilesRight[blankX + 1][blankY] = 0;
+            duplicateTilesRight[blankX][blankY] = swapVal;
+            Board boardRight = new Board(duplicateTilesRight);
+            neighborFriends.add(boardRight);
+
+        }
+        return neighborFriends;
     }
+
 
     private boolean swap(int i, int j, int it, int jt) {
         if (it < 0 || it >= n || jt < 0 || jt >= n) {
@@ -209,9 +227,9 @@ public class Board {
     private static int[] boardToArray(int[][] A) {
         // put in separate array for solvable method
         int i = 0;
-        int [] tilesArr = new int[A.length * A.length];
-        for (int[] row: A) {
-            for (int item: row) {
+        int[] tilesArr = new int[A.length * A.length];
+        for (int[] row : A) {
+            for (int item : row) {
                 if (item != 0) {
                     tilesArr[i] = item;
                     i++;
